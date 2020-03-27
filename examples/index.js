508a9b9d13f16f9853d3e34743f1b2f9cda2831f
gtinoco <genaro_13@hotmail.es>
2020-03-26 23:17:41 -0600

    First version of simple math module

---
+++ examples/index.js
@@ -0,0 +1,13 @@
+const lib = require('./../index')
+
+console.log('addition', lib.addition(1,3));
+console.log('addition', lib.addition(91,3));
+
+console.log('subtraction', lib.subtraction(1,3));
+console.log('subtraction', lib.subtraction(15,3));
+
+console.log('multiplication', lib.multiplication(1,3));
+console.log('multiplication', lib.multiplication(0.5,3));
+
+console.log('division', lib.division(1,3));
+console.log('division', lib.division(90,3));
---
+++ index.js
@@ -0,0 +1,47 @@
+/**
+ * Two numbers addition
+ * @example
+ *  n1 = 1, n2 = 4 = >  result 5
+ * @param {number} n1 Number one of the addition
+ * @param {number} n2 Number two of the addition
+ * @returns {*}
+ */
+exports.addition = (n1, n2) => {
+    return n1 + n2
+}
+
+/**
+ * Two numbers subtraction
+ * @example
+ *  n1 = 1, n2 = 4 = >  result -3
+ * @param {number} n1 Number one of the subtraction
+ * @param {number} n2 Number two of the subtraction
+ * @returns {*}
+ */
+exports.subtraction = (n1, n2) => {
+    return n1 - n2
+}
+
+/**
+ * Two numbers multiplication
+ * @example
+ *  n1 = 1, n2 = 4 = >  result 4
+ * @param {number} n1 Number one of the multiplication
+ * @param {number} n2 Number two of the multiplication
+ * @returns {*}
+ */
+exports.multiplication = (n1, n2) => {
+    return n1 * n2
+}
+
+/**
+ * Two numbers division
+ * @example
+ *  n1 = 1, n2 = 4 = >  result 0.25
+ * @param {number} n1 Number one of the division
+ * @param {number} n2 Number two of the division
+ * @returns {*}
+ */
+exports.division = (n1, n2) => {
+    return n1 / n2
+}
